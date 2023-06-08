13e480a1f99201a7ff2de90db8313fc75c8f6b8d
sakshi0009 <sakshigome1809@gmail.com>
2023-06-09 01:46:36 +0530

    Array Questions

---
+++ src/Array_01/Program01.java
@@ -0,0 +1,11 @@
+package Array_01;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+        int arr[] = new int[]{10,20,30,40,50};
+        for(int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
